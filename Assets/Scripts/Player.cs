8e519ca05ecd12f22af369f876e5c775e90d6373
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-02-16 09:45:57 -0500

    Crear propiedades publicas y privadas de nuestro Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	//Propiedades publicas
+    public int lives=3;
+    public bool isGrounded=false;
+    public bool isMooving=false;
+    public bool isInmune=false;
+
+    public float speed=5f;
+    public float jumpForce=3f;
+    public float movHor;
+
+    public float inmuneTimeCnt=0f;
+    public float inmuneTime=0.5f;
+
+    //Obtener el escenario en un mismo layer
+    public LayerMask groundLayer;
+    
+    //Para ver si colisiona
+    public float radious=0.3f;
+    public float groundRayDist=0.5f;
+
+    //Propiedades privadas
+    private Rigidbody2D rigidbody;
+    private Animator anim;
+    private SpriteRenderer spr;
+
     // Start is called before the first frame update
     void Start()
     {
