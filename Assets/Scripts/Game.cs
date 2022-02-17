e83f6db1b2a8e474a983fa569a5fa67233f7e477
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-02-17 01:43:22 -0500

    Crear el script Game, que me ayudara a manejar scenas e items

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public static Game obj;
+
+    public int maxLives=3;
+
+    public bool gamePaused=false;
+    public int score=0;
+
+    void Awake()
+    {
+        obj=this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePaused=false;
+    }
+
+    public void addScored(int scoreGive)
+    {
+        score+=scoreGive;
+    }
+
+    //reiniciar al escena actual
+    public void gameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void OnDestroy()
+    {
+        obj=null;
+    }
+}
